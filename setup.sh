dd6baba0e738ad8cdc6b60a56ba06d9bb237139b
HarshavardhanAnne <harshanne1996@gmail.com>
2018-08-08 16:33:03 -0400

    setup.sh: added the symbolic linking file and code. Added the hwclock file and code. Added the code to modify the /boot/config.txt and add in the dtoverlay for the ds3231. Still need to add the usb automount install. Created a new directory called setup_files which contains the files used in setup.sh.

--- setup.sh
+++ setup.sh
@@ -6,7 +6,28 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-#
+#Usb automount installation
+
+#Bind aethlabs ma200 serial cable to symbolic link
+cp ./setup_files/99-usb-serial.rules /etc/udev/rules.d/
+
+#Load the new rule
+sudo udevadm trigger
+
+#Check if symbolic link was created
+if [ -e /dev/aethlabs ]
+then
+  echo "Symbolic link created for aethlabs device."
+else
+  echo "Unable to create symbolic link"
+fi
+
+#Overwrite existing hwclock-set file with modified hwclock-set file
+sudo chmod +x hwclock-set
+sudo /bin/cp -rf hwclock-set /lib/udev/
+
+#Modify /boot/config.txt and add dtoverlay
+sudo echo "dtoverlay=i2c-rtc,ds3231" >> /boot/config.txt
 
 #Adafruit_MCP3008 installation
 sudo apt-get update
@@ -18,8 +39,6 @@ sudo git submodule init
 sudo git submodule update
 
 #Install the am2315 python library
-cd ./lib/am2315
-sudo python setup.py install
-cd ../../
+sudo python ./lib/am2315/setup.py install
 
 ###END####
