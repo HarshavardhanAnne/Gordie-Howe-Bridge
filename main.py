acde65ad04872d7cdb38add0059c18069098d5c2
HarshavardhanAnne <harshanne1996@gmail.com>
2018-08-09 14:43:23 -0400

    Make the PATH_TO_USB directory before using it.

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ if len(usb_paths) > 0:
 else:
     PATH_TO_USB = './' + time.strftime("%Y%m%d-%H%M%S") + '/'
 
+os.makedirs(PATH_TO_USB)
+
 #Redirection all print statments to a log file
 timestr = PATH_TO_USB + time.strftime("%Y%m%d-%H%M%S") + "_debug.log"
 sys.stdout = open(timestr,"w")
