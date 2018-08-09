3f4aafd0bad350651261caa11b74dae2b171bcef
HarshavardhanAnne <harshanne1996@gmail.com>
2018-08-09 16:14:58 -0400

    Still fixing os.makedirs problem

--- main.py
+++ main.py
@@ -19,11 +19,10 @@ usb_paths = getdevices.serial_ports('/dev/sd*')
 for i in usb_paths:
     print i
 
-
-local_dir = "./"
-PATH_TO_USB = os.path.join(local_dir,time.strftime("%Y-%m-%d_%H-%M-%S")
-
-if len(usb_paths) > 0:
+if len(usb_paths) == 0:
+    local_dir = "./"
+    PATH_TO_USB = os.path.join(local_dir,time.strftime("%Y-%m-%d_%H-%M-%S")
+else:
     usb_dir = "/media/usb/"
     PATH_TO_USB = os.path.join(usb_dir,time.strftime("%Y-%m-%d_%H-%M-%S"))
 
