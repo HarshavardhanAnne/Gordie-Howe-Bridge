5cc5e139310d9aeab16035cd4d428e53d9847cb0
HarshavardhanAnne <harshanne1996@gmail.com>
2018-08-15 18:01:45 -0400

    Added print statement to output the analog channel digital values and the calculated floating point analog values.

--- main.py
+++ main.py
@@ -194,6 +194,7 @@ def main_thread():
     avoltage_co2 = float(ch0/1024.00 * 5.0)
     ch2 = mcp.read_adc(ADC_FLOW_PIN)
     avoltage_flow = float(ch2/1024.00 * 5.0)
+    print ("Ch0 = %d, ACO2 = %0.1f, Ch2 = %d, AFLOW = %0.1f\n" % (ch0,avoltage_co2,ch2,avoltage_flow))
     temperature, humidity, crc_check = am.sense()
     d = datetime.now()
     str_d = d.strftime('%Y-%m-%d,%H:%M:%S:%f')
